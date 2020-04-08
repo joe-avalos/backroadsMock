08dc96a7d909aa33fc1a898b95ba0447f0d833d3
jose <jose@cobbleweb.co.uk>
2020-04-08 15:09:02 -0500

    master - dotenv and contenful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `BackRoads`,
@@ -21,6 +25,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_ID,
+        accessToken: process.env.CONTENFUL_ACCESS_TOKEN,
       },
     },
   ],
